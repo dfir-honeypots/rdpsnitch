a99d5532af3284c95fd5020ce9e12754b4a28c69
Chapin Bryce <27cbryce@gmail.com>
2020-01-24 20:17:19 -0500

    Changed max Username length

--- rdp-snitch.py
+++ rdp-snitch.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     asns_txt_res = post_pastebin(gather.reports['asns_txt'], title_fmt+'asns.txt', 'text')
 
     top_ips = '\n'.join([x['srcIp'] for x in gather.ips[:3]])
-    top_users = '\n'.join([x['user'] for x in gather.users[:3]])
+    top_users = '\n'.join([x['user'][:15]+"..." for x in gather.users[:3]])
     top_asns = '\n'.join([x['srcASN'][:17]+"..." for x in gather.asns[:3]]) # Limit ASN Length
 
     summary = f"{datetime.now().strftime('%Y-%m-%d')} RDP #Honeypot IOCs - {gather.total:,} scans\n\nTop IPs:\n{top_ips}\n\nTop Users:\n{top_users}\n\nTop ASNs:\n{top_asns}\n\nLinks below with details. #DFIR #InfoSec"
